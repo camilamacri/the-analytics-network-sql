691495426434923e1e9930dbc5b25fd3e44dd1e2
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 19:29:37 -0300

    Update dim_mfx.sql

--- parte-3/project/retail-data-warehouse/dim/dim_mfx.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_mfx.sql
@@ -1,6 +1,10 @@
-CREATE TABLE dim.monthly_average_fx_rate
+-- Table: stg.monthly_average_fx_rate
+
+DROP TABLE IF EXISTS dim.monthly_average_fx_rate;
+
+CREATE TABLE IF NOT EXISTS dim.monthly_average_fx_rate
                  (
-                              mes                  DATE PRIMARY KEY
+                              mes_id               DATE PRIMARY KEY
                             , cotizacion_usd_peso  DECIMAL
                             , cotizacion_usd_eur   DECIMAL
                             , cotizacion_usd_uru   DECIMAL
