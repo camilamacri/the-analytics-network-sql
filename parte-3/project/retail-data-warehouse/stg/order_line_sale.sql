457cdb93cf1b60602f67d2beada209c4363c5776
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 17:09:18 -0300

    Create order_line_sale.sql

---
+++ parte-3/project/retail-data-warehouse/stg/order_line_sale.sql
@@ -0,0 +1,20 @@
+/* Crea tabla order_sales_units
+Ventas a nivel numero de orden, item.
+*/
+DROP TABLE IF EXISTS stg.order_line_sale;
+    
+CREATE TABLE stg.order_line_sale
+                 (
+                              orden      VARCHAR(10)
+                            , producto   VARCHAR(10)
+                            , tienda     SMALLINT
+                            , fecha      date
+                            , cantidad   int
+                            , venta      decimal(18,5)
+                            , descuento  decimal(18,5)
+                            , impuestos  decimal(18,5)
+                            , creditos   decimal(18,5)
+                            , moneda     varchar(3)
+                            , pos        SMALLINT
+                            , is_walkout BOOLEAN
+                 );
