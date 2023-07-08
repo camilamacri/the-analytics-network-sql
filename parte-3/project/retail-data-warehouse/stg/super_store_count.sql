4ba6435a3b83c0606d19f038e489b5178900da6e
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 17:11:37 -0300

    Create super_store_count.sql

---
+++ parte-3/project/retail-data-warehouse/stg/super_store_count.sql
@@ -0,0 +1,11 @@
+/* Crea tabla super_store_count
+Proveedor 2 de ingresos a tienda por fecha
+*/
+DROP TABLE IF EXISTS stg.super_store_count;
+    
+CREATE TABLE stg.super_store_count
+                 (
+                              tienda SMALLINT
+                            , fecha  VARCHAR(10)
+                            , conteo SMALLINT
+                 );
