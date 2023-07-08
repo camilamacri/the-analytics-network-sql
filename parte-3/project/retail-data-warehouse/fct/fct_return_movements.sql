9e59fdd86a752d9bf90240cd609553be3b3524f8
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 20:25:45 -0300

    Update fct_return_movements.sql

--- parte-3/project/retail-data-warehouse/fct/fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_return_movements.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS fct.return_movements
               	(
 		        	         orden_venta  		VARCHAR(10)
 		                 , envio 		VARCHAR(10)
-		                 , item 		VARCHAR(10)
+		                 , item 		VARCHAR(255)
 	                   , cantidad 		INT
                      , id_movimiento 	INT PRIMARY KEY
                      , desde 		VARCHAR(100)
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS fct.return_movements
 	                   , fecha 		DATE
                      , CONSTRAINT orden_venta_fk
 					             FOREIGN KEY (orden_venta)
-				  	           REFERENCES fct.order_line_sale (orden_id),
+				  	           REFERENCES fct.order_line_sale (orden_id)
 				             , CONSTRAINT item_fk
 					             FOREIGN KEY (item)
 					             REFERENCES dim.product_master (codigo_producto_id)
