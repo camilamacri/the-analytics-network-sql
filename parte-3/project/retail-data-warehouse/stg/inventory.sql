c90e43ee22d6eaff0b6331896ed4fe6de4e7ac0f
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 17:07:34 -0300

    Create inventory.sql

---
+++ parte-3/project/retail-data-warehouse/stg/inventory.sql
@@ -0,0 +1,11 @@
+/* Crea tabla inventory
+Conteo de inventario al inicio y final del dia por fecha, tienda y codigo
+*/
+CREATE TABLE stg.inventory
+                 (
+                              tienda  SMALLINT
+                            , sku     VARCHAR(10)
+                            , fecha   DATE
+                            , inicial SMALLINT
+                            , final   SMALLINT
+                 );
