5c96e61a717cea4cd540d9252283e7cdd0e1a92b
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 20:08:10 -0300

    Update dim_supplier.sql

--- parte-3/project/retail-data-warehouse/dim/dim_supplier.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_supplier.sql
@@ -4,9 +4,9 @@ DROP TABLE IS EXISTS dim.supplier;
 
 CREATE TABLE IF NOT EXISTS dim.supplier
             	(
-		                codigo_producto  SMALLINT 
+		                codigo_producto  VARCHAR(255)
 		                , nombre         VARCHAR(255)
-		                , is_primary     BOOL default false
+		                , is_primary     VARCHAR(5)
                     , CONSTRAINT codigo_producto_fk
 					            FOREIGN KEY (codigo_producto)
 					            REFERENCES dim.product_master (codigo_producto_id)
