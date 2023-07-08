8929941a5247a3df35b674a1e28ac331d53b452b
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 19:43:44 -0300

    Update dim_cost.sql

--- parte-3/project/retail-data-warehouse/dim/dim_cost.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_cost.sql
@@ -8,5 +8,8 @@ CREATE TABLE IF NOT EXISTS dim.cost
                  (
                               codigo_producto_id    VARCHAR(10) PRIMARY KEY
                             , costo_promedio_usd DECIMAL
+                            , CONSTRAINT codigo_producto_id_fk
+					                    FOREIGN KEY (codigo_producto_id)
+					                    REFERENCES dim.product_master (codigo_producto_id)
                       
                  );
