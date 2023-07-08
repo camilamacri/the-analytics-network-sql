18c1efbfa2b43c0a99b479e56b0880a89829979e
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 19:41:18 -0300

    Update fct_super_store_count.sql

--- parte-3/project/retail-data-warehouse/fct/fct_super_store_count.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_super_store_count.sql
@@ -1,9 +1,13 @@
-CREATE TABLE fct.super_store_count
+-- Table: stg.super_store_count
+
+DROP TABLE IS EXISTS fct.super_store_count;
+
+CREATE TABLE IF NOT EXISTS fct.super_store_count
                  (
-                              tienda SMALLINT PRIMARY KEY
+                              tienda_id SMALLINT PRIMARY KEY
                             , fecha  VARCHAR(10)
                             , conteo SMALLINT
-                            , CONSTRAINT tienda_fk
-					                    FOREIGN KEY (tienda)
-					                    REFERENCES dim.store_master (codigo_tienda)
+                            , CONSTRAINT tienda_id_fk
+					                    FOREIGN KEY (tienda_id)
+					                    REFERENCES dim.store_master (codigo_tienda_id)
                  );
