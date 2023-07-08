2e25b6e01d02fd3c7f73b0624e66079ded1c2907
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 17:08:04 -0300

    Create market_count.sql

---
+++ parte-3/project/retail-data-warehouse/stg/market_count.sql
@@ -0,0 +1,11 @@
+/* Crea tabla market_count
+Proveedor 1 de ingresos a tienda por fecha
+*/
+DROP TABLE IF EXISTS stg.market_count;
+    
+CREATE TABLE stg.market_count
+                 (
+                              tienda SMALLINT
+                            , fecha  INTEGER
+                            , conteo SMALLINT
+                 );
