d16ceaa9a1e78b6e4f247be96b28b27b74874650
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 18:32:53 -0300

    Create fct_inventory.sql

---
+++ parte-3/project/retail-data-warehouse/fct/fct_inventory.sql
@@ -0,0 +1,16 @@
+CREATE SCHEMA fct;
+
+CREATE TABLE fct.inventory
+                 (
+                              tienda  SMALLINT PRIMARY KEY
+                            , sku     VARCHAR(10)
+                            , fecha   DATE
+                            , inicial SMALLINT
+                            , final   SMALLINT
+                            , CONSTRAINT tienda_fk
+					                    FOREIGN KEY (tienda)
+					                    REFERENCES dim.store_master (codigo_tienda)
+					                  , CONSTRAINT sku_fk
+					                    FOREIGN KEY (sku)
+					                    REFERENCES dim.product_master (codigo_producto)
+                 );
