09ee8f28281a6def91b4401b4d04a7e2cc4a35f5
camilamacri <129023503+camilamacri@users.noreply.github.com>
2023-07-08 17:14:22 -0300

    Create return_movements.sql

---
+++ parte-3/project/retail-data-warehouse/stg/return_movements.sql
@@ -0,0 +1,16 @@
+/* Crea tabla return_movements
+Tabla devoluciones
+*/
+
+CREATE TABLE stg.return_movements
+              	(
+		                        orden_venta VARCHAR(10)
+		                        , envio VARCHAR(10)
+		                        , item VARCHAR(10)
+	                        	, cantidad INT
+                        		, id_movimiento INT
+                        		, desde VARCHAR(100)
+                        		, hasta VARCHAR(100)
+	                        	, recibido_por VARCHAR(255)
+	                        	, fecha DATE
+            	);
